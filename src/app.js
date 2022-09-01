d75a58bc4f4171b4154cd17805eb5fc5e6f4a029
Murat Menteşe <muratmentese@gmail.com>
2022-09-01 11:29:26 +0300

    Versiyon - 1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 console.log("Merhaba Kodlama.io")
 
 //JS type safe değildir
+//let değişken tanımlamak için kullanılır
 let dolarBugun = 9.30
-
 let dolarDun = 9.20
 dolarDun = "9.20"
 {
@@ -12,25 +12,22 @@ dolarDun = "9.20"
 console.log(dolarDun)
 
 const euroDun = 11.2
-//euroDun = 11
+//euroDun = 11 //Bunu bu şekilde yazamıyoruz, const sabit bir değer oluşturur sadece
 
 console.log(euroDun)
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+//Birden fazla veriyi aynı yerde tutmak istiyorum bunun için array kullanılır
+//camelCasing Değişkenin ilk harfi küçük, 2.kelimelerinin ilk harfi büyük
+//PascalCasing Değişkenin ilk ve 2.kelimelerinin ilk harfi büyük
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+//for - Döngü, Elimizdeki veriyi dolaşmaya yarar
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
+for(let i = 0; i<konutKredileri.length;i++){
     console.log("<li>"+konutKredileri[i]+"</li>")
 }
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
 console.log(konutKredileri)
